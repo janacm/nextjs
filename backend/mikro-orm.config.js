746f40b7293a253733a78aceb76640d2eb7660e7
janacm <janac.meena@gmail.com>
2022-03-01 16:45:22 -0500

    Remove dotenv-flow

--- backend/mikro-orm.config.js
+++ backend/mikro-orm.config.js
@@ -3,7 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_flow_1 = __importDefault(require("dotenv-flow"));
-dotenv_flow_1.default.config();
 const mikro_orm_1 = __importDefault(require("./config/mikro-orm"));
 exports.default = mikro_orm_1.default;
--- backend/mikro-orm.config.ts
+++ backend/mikro-orm.config.ts
@@ -1,5 +1,3 @@
-import dotenv from "dotenv-flow";
-dotenv.config();
 import config from "./config/mikro-orm";
 
 export default config;
